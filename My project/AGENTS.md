e9784b590e97d9c7e701e44f3f1613149a54aee4
Peloncito2008 <niko.blox26@gmail.com>
2026-02-17 11:27:40 -0500

    variables 3 Ultima clase de variables
    Hoy se fisicas, creamos dos cubitos, aprendimos a poner color, y añadimos otro serialfiled, y tambien aprendimos a colicionar los cubos.

--- My project/AGENTS.md
+++ My project/AGENTS.md
@@ -1,14 +1,8 @@
 <!-- UNITY CODE ASSIST INSTRUCTIONS START -->
 - Project name: My project
 - Unity version: Unity 2023.1.0f1
-- Active scene:
-  - Name: CalbaEcena
-  - Tags:
-    - Untagged, Respawn, Finish, EditorOnly, MainCamera, Player, GameController
-  - Layers:
-    - Default, TransparentFX, Ignore Raycast, Water, UI
 - Active game object:
-  - Name: GameObject
+  - Name: cube 2
   - Tag: Untagged
   - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- My project/Assets/Scenes/_Main/Scripts/Variable.cs
+++ My project/Assets/Scenes/_Main/Scripts/Variable.cs
@@ -9,17 +9,19 @@ public class NewBehaviourScript : MonoBehaviour
     // Variables
     public string myName = "Rodolfo";
    public int edad = 17;
+    public bool isBool;
 
     [SerializeField] private TMP_InputField _InputField;
     [SerializeField] private TMP_Text _saludo;
-
-
+    [SerializeField] private Renderer _cube;
+    [SerializeField] private BoxCollider _boxCollider;
 
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        _cube.material.color = Color.red;
+        _boxCollider.isTrigger = false;
     }
 
     // Update is called once per frame
